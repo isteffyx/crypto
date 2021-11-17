9bc3cc948fcb426cea6c0dce62145dcf14804474
pkumarex <PremalathaX.Kumareson@intel.com>
2021-11-17 06:06:42 -0500

    Added own crypto pkgs

---
+++ skc_rand/skc_rand.go
@@ -0,0 +1,18 @@
+// Package rand implements a cryptographically secure
+// random number generator.
+package skc_rand
+
+import (
+	"crypto/rand"
+)
+
+// Reader is a global, shared instance of a cryptographically
+// secure random number generator.
+//
+// On Linux, FreeBSD, Dragonfly and Solaris, Reader uses getrandom(2) if
+// available, /dev/urandom otherwise.
+// On OpenBSD and macOS, Reader uses getentropy(2).
+// On other Unix-like systems, Reader reads from /dev/urandom.
+// On Windows systems, Reader uses the RtlGenRandom API.
+// On Wasm, Reader uses the Web Crypto API.
+var Reader = rand.Reader
