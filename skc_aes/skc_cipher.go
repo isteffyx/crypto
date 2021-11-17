d01b9685d5797ed64c3f38ed18b290482d140fd5
pkumarex <PremalathaX.Kumareson@intel.com>
2021-11-17 05:32:57 -0500

    Add own crypto pkgs

---
+++ skc_aes/skc_cipher.go
@@ -0,0 +1,24 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package skc_aes
+
+import (
+	"crypto/cipher"
+
+	"crypto/aes"
+)
+
+// NewCipher creates and returns a new cipher.Block.
+// The key argument should be the AES key,
+// either 16, 24, or 32 bytes to select
+// AES-128, AES-192, or AES-256.
+func NewCipher(key []byte) (cipher.Block, error) {
+	return aes.NewCipher(key)
+}
+
+//getting session id
+/* func getSessionId(){
+
+} */
---
+++ skc_cipher/skc_gcm.go
@@ -0,0 +1,17 @@
+package skc_cipher
+
+import (
+	"crypto/cipher"
+)
+
+// AEAD is a cipher mode providing authenticated encryption with associated
+// data. For a description of the methodology, see
+//	https://en.wikipedia.org/wiki/Authenticated_encryption
+type AEAD = cipher.AEAD
+
+// In general, the GHASH operation performed by this implementation of GCM is not constant-time.
+// An exception is when the underlying Block was created by aes.NewCipher
+// on systems with hardware support for AES. See the crypto/aes package documentation for details.
+func NewGCM(cphr cipher.Block) (AEAD, error) {
+	return cipher.NewGCM(cphr)
+}
